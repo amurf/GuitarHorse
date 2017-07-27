72a18b78d9d50ea3705093d98207788885776757
ash <ash@strategicdata.com.au>
2017-07-27 17:25:48 +1000

    Add scale option to creator

--- creator/src/components/ComponentList.vue
+++ creator/src/components/ComponentList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <b-btn variant="primary" size="md" @click="addQuestion">Add Text</b-btn>
+    <b-btn variant="primary" size="md" @click="addText">Add Text</b-btn>
+    <b-btn variant="primary" size="md" @click="addScale">Add Scale</b-btn>
   </div>
 </template>
 
@@ -22,10 +23,20 @@ export default {
     };
   },
   methods: {
-    addQuestion: function() {
+    addText: function() {
       let question = {
         name: 'example-' + this.questionsAdded,
         component: 'ghText',
+        label: 'the example label'
+      };
+      this.$emit('question-added', question);
+      this.questionsAdded++;
+    },
+    addScale: function() {
+      let question = {
+        name: 'example-' + this.questionsAdded,
+        component: 'ghScale',
+        options: 3,
         label: 'the example label'
       };
       this.$emit('question-added', question);
@@ -36,12 +47,8 @@ export default {
 </script>
 
 <style lang="scss">
-input {
-  width: 100%;
-}
-
 button {
   width: 100%;
-  margin: 2px 0;
+  margin: 5px 0;
 }
 </style>
