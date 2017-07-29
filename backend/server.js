68ebfb6d7fa05ec4531901cf82d82edf6efb1ef2
ash <ash@strategicdata.com.au>
2017-07-29 16:11:35 +1000

    Add select component to default form.

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,16 @@ routes.forEach(route => server.route(route));
 // XXX: Temporary. Ensure we always have a form!
 const db = require('./lib/db');
 let form = {
-  config: {
-    name: "The Survey",
-  },
+  name: "The Survey",
   questions: [
     { component: 'ghText', label: "First Question", name: "qOne"},
     { component: 'ghText', label: "Second Question", name: "qTwo"},
-    { component: 'ghScale', label: "Third question", name: "qThree", options: ["one", "two", "three" ] },
+    { component: 'ghSelect', label: "Third question", name: "qThree", options: ["one", "two", "three" ] },
     { component: 'ghScale', label: "Fourth question", name: "qFour", options: 3 },
   ],
 };
-db.addForm(form).then(console.log("Added " + form.config.name));
+
+db.addForm(form).then(console.log("Added " + form.name));
 
 server.start((err) => {
   if (err) {
