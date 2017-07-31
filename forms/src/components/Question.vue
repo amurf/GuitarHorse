78c858ad1974359c63ce7615f063aac378835b43
huwmillar <huw.millar@strategicdata.com.au>
2017-07-31 12:52:07 +1000

    Added CSS styling

--- forms/src/components/Question.vue
+++ forms/src/components/Question.vue
@@ -1,9 +1,9 @@
 <template>
   <div class='question'>
     <div class='question-label'>
-      <span class='number'>{{ number }}</span>
+      <span class='question-number'>{{ number }}</span>
       <label>{{ question.label }}</label>
-      <span class='status'>status</span>
+      <span class='question-status'>* Required</span>
     </div>
     <div class='question-input'>
       <component :is="question.component" :answers="answers" :question="question"></component>
@@ -24,22 +24,47 @@ export default {
 }
 </script>
 <style lang="scss">
+
 .question {
   margin: 1em 0em;
+  border-width: $border-width;
+  border-style: $border-style;
+  border-color: $border-color;
+  border-radius: $border-radius;
 }
 
 .question-label {
-  background-color: $light-gray;
+  background-color: $lightest-gray;
   width: 100%;
   padding: 1em;
+  border-top-left-radius: $border-radius;
+  border-top-right-radius: $border-radius;
+  border-bottom:$border-color $border-style $border-width;
+
+  display:flex;
+  justify-content:space-between;
 }
 
 .question-input {
-  background-color: $not-so-light-gray;
+  background-color: $lightest-gray;
   padding: 1em;
+  border-bottom-left-radius: $border-radius;
+  border-bottom-right-radius: $border-radius;
 }
 
 .question-label label {
   display: inline;
+  width: 100%;
+  margin-left:10px;
 }
+
+.question-number {
+display:inline-block;
+
+}
+
+.question-status {
+
+}
+
 </style>
