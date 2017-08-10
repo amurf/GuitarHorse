4f16029dac2153558dc1349ddab8d7a81d47a36b
ash <ash@strategicdata.com.au>
2017-08-10 14:13:55 +1000

    Webpack optimisations

--- builder/webpack.config.js
+++ builder/webpack.config.js
@@ -1,9 +1,22 @@
 var path    = require('path')
 var webpack = require('webpack')
 
+var sassIncludes = [
+  'node_modules/font-awesome/scss',
+  'node_modules/bootstrap/scss',
+];
+
 module.exports = {
+  cache: true,
   entry: {
     app: '/build/src/app.js',
+    vendor: [
+      "vue",
+      "vue-router",
+      "axios",
+      "bootstrap-vue",
+      "vuedraggable",
+    ],
   },
   output: {
     path: '/build/app',
@@ -13,75 +26,69 @@ module.exports = {
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-            module.resource &&
-            /\.js$/.test(module.resource) &&
-            module.resource.indexOf(
-              path.join(__dirname, '../node_modules')
-              ) === 0
-            )
-      }
     }),
   ],
   module: {
     rules: [
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        loaders: {
-          'scss': [
-          { loader: "vue-style-loader" },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': [
+              { loader: "vue-style-loader" },
+              { loader: "css-loader" },
+              {
+                loader: "sass-loader",
+                options: {
+                  includePath: sassIncludes,
+                },
+              },
+              {
+                loader: 'sass-resources-loader',
+                options: {
+                  resources: '/build/src/scss/overrides.scss',
+                },
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
           { loader: "css-loader" },
-          { loader: "sass-loader" },
           {
-            loader: 'sass-resources-loader',
+            loader: "sass-loader",
             options: {
-              resources: '/build/src/scss/overrides.scss',
+              includePath: sassIncludes,
             },
           },
-          ],
-        },
+        ],
+        exclude: /(node_modules)/,
       },
-    },
-    {
-      test: /\.scss$/,
-      use: [
-      { loader: "style-loader" },
-      { loader: "css-loader" },
-      { loader: "sass-loader" },
       {
-        loader: 'sass-resources-loader',
-        options: {
-          resources: '/build/src/scss/overrides.scss',
-        },
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
       },
-      ],
-      exclude: /(node_modules)/,
-    },
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /(node_modules)/,
-    },
-    {
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'img/[name].[hash:7].[ext]',
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'img/[name].[hash:7].[ext]',
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]',
+        }
       }
-    },
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'fonts/[name].[hash:7].[ext]',
-      }
-    }
     ]
   },
   resolve: {
@@ -97,21 +104,21 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: '"production"'
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          sourceMap: true,
-          compress: {
-            warnings: false
-          }
-        }),
-        new webpack.LoaderOptionsPlugin({
-          minimize: true
-        })
-    ])
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
 }
