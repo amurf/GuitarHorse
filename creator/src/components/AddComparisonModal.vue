3f1fc64259550c69193974677dfe431db348502c
ash <ash@strategicdata.com.au>
2017-09-14 21:56:09 +1000

    WIP comparison modal
    Need to get the full question list from somewhere for these
    comparisons...

---
+++ creator/src/components/AddComparisonModal.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<b-button @click="modalShow = !modalShow">
+			Open Modal
+		</b-button>
+		<b-modal v-model="modalShow">
+			Hello From Modal!
+		</b-modal>
+	</div>
+</template>
+<script>
+export default {
+	name: 'add-comparison-modal',
+	props: ['comparisons', 'questions'],
+	data() {
+		return {
+			modalShow: false,
+		};
+	},
+}
+</script>
