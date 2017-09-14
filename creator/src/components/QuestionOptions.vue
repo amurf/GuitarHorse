35e73dce4479edaeadc033327fb0d3e838d3a007
ash <ash@strategicdata.com.au>
2017-09-14 21:55:38 +1000

    Remove one layer of un-needed component

--- creator/src/components/QuestionOptions.vue
+++ creator/src/components/QuestionOptions.vue
@@ -1,87 +1,102 @@
 <template>
-  <div class='question-options'>
-    <gh-question-config-form :questions="formConfig.questions" :model="model"></gh-question-config-form>
-  </div>
+    <div class='question-options'>
+        <div v-for="question in formConfig.questions" class="question-options__config">
+            <label>{{ question.label }}</label>
+            <component :is="question.component" :answers="model" v-bind="question">
+            </component>
+
+        </div>
+        <!-- display these nicer -->
+        <p>comparisons: <code>{{ model.comparisons }}</code></p>
+        <gh-add-comparison-modal></gh-add-comparison-modal>
+    </div>
 </template>
 <script>
 
-import { ghQuestionConfigForm } from "shared/components/layouts";
+import ghAddComparisonModal from './AddComparisonModal';
 import ghQuestion from "shared/components/Question";
+import ghText from "shared/components/Text";
+import ghNumber from "shared/components/Number";
+import ghCheckbox from "shared/components/Checkbox";
+import ghSelect from "shared/components/Select";
+
+import Validators from "shared/validators";
 
 export default {
-  name: 'gh-question-options',
-  props: ['component', 'model'],
-  components: { ghQuestionConfigForm },
-  data() {
-    return {
-      formConfig: {
-        questions: [],
-      },
-    };
-  },
-  methods: {
-    // Stolen from vuejs prop inspection code.
-    getType(fn) {
-      const match = fn && fn.toString().match(/^\s*function (\w+)/)
-      return match ? match[1] : ''
+    name: 'gh-question-options',
+    props: ['component', 'model'],
+    //components: { ghQuestionConfigForm },
+    components: { ghText, ghNumber, ghCheckbox, ghSelect, ghAddComparisonModal },
+    data() {
+        return {
+            formConfig: {
+                questions: [],
+            },
+        };
     },
-  },
-  created() {
-    let vm = this; // forEach loop overwrites this
+    methods: {
+        // Stolen from vuejs prop inspection code.
+        getType(fn) {
+            const match = fn && fn.toString().match(/^\s*function (\w+)/)
+            return match ? match[1] : ''
+        },
+    },
+    created() {
+        let vm = this; // forEach loop overwrites this
 
-    let componentObject = ghQuestion.components[this.component];
-    const props         = componentObject.props;
+        let componentObject = ghQuestion.components[this.component];
+        const props         = componentObject.props;
 
-    let typeToComponents = {
-      'String'  : 'ghText',
-      'Boolean' : 'ghCheckbox',
-      'Number'  : 'ghNumber',
-      'Array'   : 'ghText',
-      //'Array'   : 'tbc',
-    };
-
-    // Populated with defaults that apply to every single field
-    let questions = [{
-      label: 'Label',
-      component: 'ghText',
-      name: 'label',
-    }];
-
-    Object.keys(componentObject.props).forEach(function(propName) {
-      let type = props[propName].type;
-      let label = props[propName].label;
-
-      // Skip anything without a type property
-      // Maybe this should look at the label property?
-      if (label) {
-        let stringifiedType = vm.getType(type);
-        let newQuestion = {
-          label: label,
-          component: typeToComponents[stringifiedType],
-          name: propName,
+        let typeToComponents = {
+            'String'  : 'ghText',
+            'Boolean' : 'ghCheckbox',
+            'Number'  : 'ghNumber',
+            'Array'   : 'ghText',
+            //'Array'   : 'tbc',
         };
 
-        // Better this? Booleans need options.
-        if (stringifiedType == 'Boolean') {
-          newQuestion.options = [""];
-        }
+        // Populated with defaults that apply to every single field
+        let questions = [{
+            label: 'Label',
+            component: 'ghText',
+            name: 'label',
+        }];
 
-        questions.push(newQuestion);
-      }
-    });
+        Object.keys(componentObject.props).forEach(function(propName) {
+            let type = props[propName].type;
+            let label = props[propName].label;
 
-    vm.formConfig.questions = questions;
-  },
+            // Skip anything without a type property
+            // Maybe this should look at the label property?
+            if (label) {
+                let stringifiedType = vm.getType(type);
+                let newQuestion = {
+                    label: label,
+                    component: typeToComponents[stringifiedType],
+                    name: propName,
+                };
+
+                // Better this? Booleans need options.
+                if (stringifiedType == 'Boolean') {
+                    newQuestion.options = [""];
+                }
+
+                questions.push(newQuestion);
+            }
+        });
+
+        vm.formConfig.questions = questions;
+    },
 }
 </script>
 <style lang="scss" scoped>
 
 .question-options {
-  border-width: $border-width;
-  border-style: $border-style;
-  border-color: $border-color;
-  border-radius: $border-radius;
-  background-color: $lightest-gray;
+    border-width: $border-width;
+    border-style: $border-style;
+    border-color: $border-color;
+    border-radius: $border-radius;
+    background-color: $lightest-gray;
 }
 
 </style>
