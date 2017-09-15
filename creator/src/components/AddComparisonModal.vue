0222d0ec7b490c76a9cfd8b10981ab38dbc86a8f
ash <ash@strategicdata.com.au>
2017-09-15 13:23:12 +1000

    WIP - Add comparisons on logic page works now!

--- creator/src/components/AddComparisonModal.vue
+++ creator/src/components/AddComparisonModal.vue
@@ -1,19 +1,17 @@
 <template>
     <div>
-        <b-button @click="modalShow = !modalShow">
-            Open Modal
+        <b-button @click="modalShow = !modalShow" variant="info" type="md">
+            Add Comparison
         </b-button>
-        <b-modal v-model="modalShow" title="Add new comparison" ok-title="Add Comparison">
-            <label>
-                Validator
-                <gh-select :options="validators" :answers="newComparison" name="type">
-                </gh-select>
-            </label>
-            <label>
-                Question name
-                <gh-select :options="questionNames" :answers="newComparison" name="slot">
-                </gh-select>
-            </label>
+        <b-modal v-model="modalShow" title="Add new comparison" ok-title="Add Comparison" @ok="addComparison()">
+            <!-- This would be better as an autocomplete widget -->
+            <gh-select :options="questionNames" :answers="comparisonQuestion" name="name">
+            </gh-select>
+            <gh-select :options="validators" :answers="newComparison" name="type">
+            </gh-select>
+            <!-- This would be better as an autocomplete widget -->
+            <gh-select :options="questionNames" :answers="newComparison" name="slot">
+            </gh-select>
             <gh-question v-if="newComparison.slot" number="1" :question="questionsByName[newComparison.slot]" :answers="answer" disable-validations="true">
             </gh-question>
         </b-modal>
@@ -26,24 +24,29 @@ import Validators from 'shared/validators';
 import { keyBy } from 'lodash';
 export default {
     name: 'add-comparison-modal',
-    props: ['comparisons', 'questions'],
+    props: ['comparisons', 'questionsByName'],
     components: { ghSelect, ghQuestion },
     data() {
         return {
             modalShow: false,
             newComparison: {},
             answer: {},
+            comparisonQuestion: {},
         };
     },
     computed: {
         validators() {
             return Object.keys(Validators);
         },
-        questionsByName() {
-            return keyBy(this.questions, 'name');
-        },
         questionNames() {
             return Object.keys(this.questionsByName);
+        },
+    },
+    methods: {
+        addComparison() {
+            // TODO: Extend this to handle more cases, like...
+            // requiredIf something == 55
+            this.$emit('add-comparison', this.comparisonQuestion.name, this.newComparison);
         },
     },
 }
--- creator/src/components/FormBuilderLogic.vue
+++ creator/src/components/FormBuilderLogic.vue
@@ -1,20 +1,46 @@
 <template>
-  <div class="tab__container">
-    <div>
-      <h1>Logic</h1>
+    <div class="tab__container">
+        <div>
+            <h1>Logic</h1>
+            <add-comparison-modal :questions-by-name="questionsByName" @add-comparison="addComparison">
+            </add-comparison-modal>
+            <br>
+            {{ form.questions }}
+            <br>
+            <b-card-group deck v-for="question in allComparisons" :key="question.id">
+                <b-card v-for="(comparison, index) in question.comparisons" :key="`${question.id}-${index}`">
+                    {{ question.name }} {{ comparison.type }} {{ comparison.slot }}
+                </b-card>
+            </b-card-group>
 
-      <add-comparison-modal :questions="form.questions">
-      </add-comparison-modal>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 import AddComparisonModal from './AddComparisonModal';
-
+import { filter, keyBy } from 'lodash';
 export default {
-  name: 'form-builder-logic',
-  components: { AddComparisonModal },
-  props: ['form'],
+    name: 'form-builder-logic',
+    components: { AddComparisonModal },
+    props: ['form'],
+    computed: {
+        allComparisons() {
+            return filter(this.form.questions, qn => qn.comparisons);
+        },
+        questionsByName() {
+            return keyBy(this.form.questions, 'name');
+        },
+    },
+    methods: {
+        addComparison(questionName, newComparison) {
+            let question = this.questionsByName[questionName];
+            if (!question.comparisons) {
+                this.$set(question, 'comparisons', []);
+            }
+
+            question.comparisons.push(newComparison);
+        },
+    },
 }
 </script>
 <style lang="scss">
